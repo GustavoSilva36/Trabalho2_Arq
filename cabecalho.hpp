41e6616bd87f8fee53bf10a92f8e85bdb0ed7541
GustavoSilva36 <107961543+GustavoSilva36@users.noreply.github.com>
2023-02-21 18:05:56 -0300

    Add files via upload

--- cabecalho.hpp
+++ cabecalho.hpp
@@ -4,31 +4,26 @@
 
 using namespace std;
 
-typedef unsigned int registrador; // 4 bytes ou 32 bits
-typedef char byte; // 1 byte ou 8 bits
-typedef bool bit; // 1 bit (tem opção melhor?)
-typedef short bit16; // 16 bits
-
-struct BancoRegistradores{
-    registrador *bancoReg;
-
-    BancoRegistradores(){
-        bancoReg = new registrador[32];
-        clear();
-    }
-    ~BancoRegistradores(){
-        delete [] bancoReg;
-    }
-    void clear(){
-        for(int i=0; i<32; i++)
-            bancoReg[i] = 0;
-    }
-    registrador acessarRegistrador(bit endereco[]){
-        unsigned short indice = 0;
-        for(int i=0; i<5; i++)
-            if(endereco[i])
-                indice += pow(2, 4-i);
-        cout << indice << endl;
-        return bancoReg[indice];
-    }
+class BancoRegistradores{
+    private:
+        bitset<32> *registradores;
+    
+    public:
+        BancoRegistradores(){
+            registradores = new bitset<32>[32];
+            clear();
+        }
+        ~BancoRegistradores(){
+            delete [] registradores;
+        }
+        void clear(){
+            for(int i=0; i<32; i++)
+                registradores[i] = 0;
+        }
+        bitset<32> getRegistrador(bitset<5> endereco){
+            return registradores[endereco.to_ulong()];
+        }
+        void setRegistrador(bitset<5> endereco, bitset<32> valor){
+            registradores[endereco.to_ulong()] = valor;
+        }
 };
