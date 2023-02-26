943f12adbcde15d1dd0d1abb79a5cc3fc9569a8e
zSchwi <122842926+zSchwi@users.noreply.github.com>
2023-02-25 21:52:42 -0300

    Update instructionDecoder.cpp
    colocando impressão de tela e passando branch para bitset

--- instructionDecoder.cpp
+++ instructionDecoder.cpp
@@ -1,90 +1,102 @@
 #ifndef _CABECALHO_H
-	#include "cabecalho.hpp"
+#include "cabecalho.hpp"
 #endif
 
 struct ControlUnit
 {
     // inicialização da unidade de controle
     bool jump = false;
-    bool branch = false;
+    bitset<2> branch;
     bool mem_read = false;
-    bool mem_to_reg = false;
+    bool mem_reg = false;
     bool mem_write = false;
     bool alu_src = false;
     bool reg_write = false;
     bool reg_dst = false;
-    bool isBNE = false;
+    bitset<2> retornoAlu;
 
-    bitset<2> setControlUnit(bitset<6> opcode); // retirei a funct por enquanto
+    void setControlUnit(bitset<6> opcode); // retirei a funct por enquanto
+    void imprimirID()
+    {
+        cout << "jump: " << jump << endl
+             << "branch: " << branch << endl
+             << "mem_read: " << mem_read << endl
+             << "mem_reg: " << mem_reg << endl
+             << "mem_write: " << mem_write << endl
+             << "alu_src: " << alu_src << endl
+             << "reg_write: " << reg_write << endl
+             << "reg_dst: " << reg_dst << endl
+             << "retornoAlu: " << retornoAlu << endl;
+    }
 };
 
-bitset<2> ControlUnit::setControlUnit(bitset<6> opcode) // retirei a funct por enquanto
+void ControlUnit::setControlUnit(bitset<6> opcode) // retirei a funct por enquanto
 {
     // Instruções tipo R:
     if (opcode == bitset<6>("000000"))
     {
-        cout << "Instrucao do Tipo R" << endl;
-
         // talvez mult e div não estejam sendo contempladas..
         reg_write = true;
         reg_dst = true;
         // retorno pra AlUCtr
-        return bitset<2>("10");
+        retornoAlu = bitset<2>("10");
+        imprimirID();
     }
 
     // Instruções tipo branch (beq / bne / bge):
     else if (opcode == bitset<6>("000100") or opcode == bitset<6>("000101") or opcode == bitset<6>("000110"))
     {
-        cout << "Instrucao do Tipo Branch" << endl;
+        if (opcode == bitset<6>("000101")) // bne
+            branch = bitset<2>("10");
 
-        if (opcode == bitset<6>("000101"))
-            isBNE = true;
-        branch = true;
-        // retorno pra AlUCtr
-        return bitset<2>("01");
+        else if (opcode == bitset<6>("000110")) // bge
+            branch = bitset<2>("11");
+        else
+        {
+            branch = bitset<2>("01");
+        }
+
+        retornoAlu = bitset<2>("01");
+
+        imprimirID();
     }
     // Instruções tipo J
 
     else if (opcode == bitset<6>("000010") or opcode == bitset<6>("000011") or (opcode == bitset<6>("000000"))) // retirei a funct por enquanto
     {
-        cout << "Instrucao do Tipo Jump" << endl;
-
         jump = true;
-        return bitset<2>("11");
+        imprimirID();
     }
 
     // Instrução Load Word
     else if (opcode == bitset<6>("100011"))
     {
-        cout << "Instrucao do Tipo Read" << endl;
-
         mem_read = true;
-        mem_to_reg = true;
+        mem_reg = true;
         alu_src = true;
         reg_write = true;
 
-        return bitset<2>("00");
+        retornoAlu = bitset<2>("00");
+        imprimirID();
     }
 
-    else if (opcode == bitset<6>("101011"))
+    else if (opcode == bitset<6>("101011")) // Instrução Save Word
     {
-        cout << "Instrucao do Tipo Write" << endl;
-
         mem_write = true;
         alu_src = true;
 
-        return bitset<2>("00");
+        retornoAlu = bitset<2>("00");
+        imprimirID();
     }
 
     else
     // Instrução tipo I:
     {
-        cout << "Instrucao do Tipo Immediate" << endl;
-
         alu_src = true;
         reg_write = true;
 
-        return bitset<2>("00");
+        retornoAlu = bitset<2>("00");
+        imprimirID();
     }
 }
 
@@ -94,26 +106,25 @@ bitset<2> ControlUnit::setControlUnit(bitset<6> opcode) // retirei a funct por e
 
 class ID : public ControlUnit
 {
-    public:
-        bitset<2> retornoAlu;
-        bitset<32> value_Rs;
-        bitset<32> value_Rt;
-        bitset<5> Write_Adrr;
+public:
+    bitset<32> value_Rs;
+    bitset<32> value_Rt;
+    bitset<5> Write_Adrr;
 
-        ID(bitset<6> opcode, bitset<5> rs, bitset<5> rt, bitset<5> rd, BancoRegistradores &BR)
+    ID(bitset<6> opcode, bitset<5> rs, bitset<5> rt, bitset<5> rd, BancoRegistradores &BR)
+    {
+        // falta entender isso aqui, saber oque pegar e como retornar o conteudo do registrador
+        setControlUnit(opcode);
+        value_Rs = BR.getRegistrador(rs);
+        value_Rt = BR.getRegistrador(rt);
+        if (reg_dst)
         {
-            // falta entender isso aqui, saber oque pegar e como retornar o conteudo do registrador
-            retornoAlu = setControlUnit(opcode);
-            value_Rs = BR.getRegistrador(rs);
-            value_Rt = BR.getRegistrador(rt);
-            if (reg_dst)
-            {
-                Write_Adrr = rd;
-            }
-
-            else
-            {
-                Write_Adrr = rt;
-            }
+            Write_Adrr = rd;
         }
+
+        else
+        {
+            Write_Adrr = rt;
+        }
+    }
 };
